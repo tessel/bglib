ef52a746c37f57f7e39b8544d5d1235a8be782f7
johnnyman727 <jonathan.mckay@students.olin.edu>
2013-11-11 14:11:38 -0800

    Fixed param codes

--- BGLibEvents/BGLibEvents.js
+++ BGLibEvents/BGLibEvents.js
@@ -1,5 +1,10 @@
 var bitwise = require('../BitwiseOperations/BitwiseOperations');
 
+/***************************************
+*		SYSTEM RESPONSES
+****************************************/ 
+
+
 var _bgEventPortStatusChange = function(params){
 	this.timestamp = bitwise.numberFromUint8Bytes([params[0], params[1], params[2], params[3]])
 	this.port = params[4];
@@ -8,18 +13,15 @@ var _bgEventPortStatusChange = function(params){
 }	
 
 var Events = {
-	7 : [_bgEventPortStatusChange],
-}
-
-var EventNames = {
-	0 : [],
-	1 : [],
-	2 : [],
-	3 : [],
-	4 : [],
-	5 : [],
-	6 : [],
-	7 : ["portEventStatusChange"],
+	0: [],
+	1: [],
+	2: [],
+	3: [],
+	4: [],
+	5: [],
+	6: [],
+	7: [_bgEventPortStatusChange],
+	8: [],
 }
 	
 module.exports.Events = Events;
