61f6a0216b0c4ca191c77168b677c8e338b3e0a3
johnnyman727 <johnnyman727@gmail.com>
2014-11-07 13:20:42 +0100

    Changes getPacket logic to return synchronously

--- test/test.js
+++ test/test.js
@@ -17,32 +17,27 @@ bglib.setPacketMode(1);
 assert(bglib, "BGLib could not be imported...");
 
 // Packet Creation:
-bglib.getPacket(bg.api.systemHello, function(err, response) {
-	assert(!err, "Error creating the simple hello packet.");
-	assert(response.cID == 1 
-		&& response.cClass == 0, "Invalid packet created.");
-	console.log("Simple Packet Creation Passed.")
-});
+var response = bglib.getPacket(bg.api.systemHello);
+assert(response.cID == 1 
+	&& response.cClass == 0, "Invalid packet created.");
+console.log("Simple Packet Creation Passed.")
 
-bglib.getPacket(bg.api.gapConnectDirect, [new Buffer([108, 189, 40, 93, 28, 216]), 1, 25, 50, 500, 8], function(err, response) {
-	assert(!err, "Error creating the complex connect packet.");
-	assert(response.cID == 3 
-		&& response.cClass == 6, "Invalid packet created.");
-	console.log("Packet", response);
-	console.log("Complex Packet Creation Passed.");
-});
-
-bglib.getPacket(bg.api.systemHello, ["Invalid", "params"], function(err, response) {
-	assert(err, "Unable to detect invalid number of parameters.");
-	console.log("Detecting Too Few Parameters Passed.")
-});
+response = bglib.getPacket(bg.api.gapConnectDirect, [new Buffer([108, 189, 40, 93, 28, 216]), 1, 25, 50, 500, 8]);
+assert(response.cID == 3 
+	&& response.cClass == 6, "Invalid packet created.");
+console.log("Complex Packet Creation Passed.");
 
 assert.throws(function() {
-	bglib.getPacket(bg.api.gapConnectDirect, [108, 189, 40, 93, 28, 216], 1, 25, 50, 500, 8, function(err, response) {});
-	}
-, "Did not throw an error for too many parameters being passed in.");
-console.log("Detecting Too Many Parameters Passed.");
+	bglib.getPacket(bg.api.systemHello, ["Invalid", "params"])
+	console.log("Invalid parameters FAILED.");
+});
+console.log("Detecting Invalid Parameters Passed.");
 
+assert.throws(function() {
+	bglib.getPacket(bg.api.gapConnectDirect, [108, 189, 40, 93, 28, 216], 1, 25, 50, 500, 8);
+	console.log("Invalid parameters FAILED.");
+});
+console.log("Detecting Too Many Parameters Passed.");
 
 // Packet Parsing:
 var incoming = new Buffer([128, 38, 6, 0, 185, 0, 108, 189, 40, 93, 28, 216, 1, 255, 27, 2, 1, 6, 17, 6, 186, 86, 137, 166, 250, 191, 162, 189, 1, 70, 125, 110, 56, 88, 171, 173, 5, 22, 10, 24, 7, 4, 128, 38, 6, 0, 181, 0, 108, 189, 40, 93, 28, 216, 1, 255, 27, 2, 1, 6, 17, 6, 186, 86, 137, 166, 250, 191, 162, 189, 1, 70, 125, 110, 56, 88, 171, 173, 5, 22, 10, 24, 7, 4, 128, 38, 6, 0, 191, 0, 108, 189, 40, 93, 28, 216, 1, 255, 27, 2, 1, 6, 17, 6, 186, 86, 137, 166, 250, 191, 162, 189, 1, 70, 125, 110, 56, 88, 171, 173, 5, 22, 10, 24, 7, 4, 128, 38, 6, 0, 182, 0, 108, 189, 40, 93, 28, 216, 1, 255, 27, 2, 1, 6, 17, 6, 186, 86, 137, 166, 250, 191, 162, 189, 1, 70, 125, 110, 56, 88, 171, 173, 5, 22, 10, 24, 7, 4, 128, 38, 6, 0, 179, 0, 108, 189, 40, 93, 28, 216, 1, 255, 27, 2, 1, 6, 17, 6, 186, 86, 137, 166, 250, 191, 162, 189, 1, 70, 125, 110, 56, 88, 171, 173, 5, 22]);
